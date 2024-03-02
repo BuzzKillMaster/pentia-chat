62f83f60e790b9ffbb3921388319bae6220505fd
Christian Pedersen <chris@buzzkill.dev>
2024-03-02 09:50:34 +0100

    Add user notification preferences to ChatMessageInputField
    The user now gets prompted to accept or deny receiving notifications from a room that they've actively participated (sent a message) in for the first time. Async storage has been used as a temporary measure.

--- src/components/ChatMessageInputField.tsx
+++ src/components/ChatMessageInputField.tsx
@@ -1,7 +1,9 @@
-import {ReactElement, useContext, useState} from "react";
+import {ReactElement, useContext, useEffect, useState} from "react";
 import {Alert, StyleSheet, TextInput} from "react-native";
 import {SessionContext} from "../providers/SessionProvider";
 import firestore from "@react-native-firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Notifications from 'expo-notifications';
 
 /**
  * Renders an input field for sending messages in a chat group.
@@ -14,6 +16,22 @@ export default function ChatMessageInputField({group}: { group: string | undefin
     const {user} = useContext(SessionContext)
 
     const [messageContents, setMessageContents] = useState<string>("")
+    const [userWantsNotifications, setUserWantsNotifications] = useState<boolean | undefined | null>()
+
+    useEffect(() => {
+        (async () => {
+            const wantsNotifications = await AsyncStorage.getItem("userWantsNotifications")
+
+            switch (wantsNotifications) {
+                case "true":
+                case "false":
+                    setUserWantsNotifications(wantsNotifications === "true")
+                    break
+                default:
+                    setUserWantsNotifications(null)
+            }
+        })()
+    }, [])
 
     /**
      * Sends a message to a chat group, using the current user's information.
@@ -36,7 +54,46 @@ export default function ChatMessageInputField({group}: { group: string | undefin
                 created_at: firestore.FieldValue.serverTimestamp()
             }).catch(_ => {
             Alert.alert("Uh-oh!", "It looks like we're having trouble sending your message. Please try again later.")
-        }).then(_ => setMessageContents(""))
+        }).then(_ => {
+            setMessageContents("")
+            checkUserWantsNotifications()
+        })
+    }
+
+    /**
+     * Checks if the user wants to receive notifications for new messages in a chat group.
+     * If the user's preference is not set, it shows an alert asking for their preference and updates the preference accordingly.
+     *
+     * @returns {Promise<void>} - A promise that resolves once the user's preference is set.
+     */
+    const checkUserWantsNotifications = async (): Promise<void> => {
+        if (userWantsNotifications !== null) return
+
+        Alert.alert("Hey there!", "We noticed you haven't set your notification preferences yet. Would you like to receive notifications for new messages in this chat group?", [
+            {
+                text: "Yes, please!",
+                onPress: async () => {
+                    const permission = await Notifications.requestPermissionsAsync()
+
+                    if (permission.status === "granted") {
+                        await AsyncStorage.setItem("userWantsNotifications", "true")
+                        setUserWantsNotifications(true)
+                    } else if (permission.status === "denied") {
+                        await AsyncStorage.setItem("userWantsNotifications", "false")
+                        setUserWantsNotifications(false)
+
+                        Alert.alert("Uh-oh!", "It looks like you've denied notifications. You can change this in your device's settings.")
+                    }
+                }
+            },
+            {
+                text: "No, thanks.",
+                onPress: async () => {
+                    await AsyncStorage.setItem("userWantsNotifications", "false")
+                    setUserWantsNotifications(false)
+                }
+            }
+        ])
     }
 
     return (
