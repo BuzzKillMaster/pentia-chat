50d37b8b0273089afc15cf937376e294aa98536a
Christian Pedersen <chris@buzzkill.dev>
2024-03-02 13:09:45 +0100

    Render chat message based on message type
    The messages are now rendered differently depending on their type of content, instead of always having their contents output as strings.

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,6 +1,7 @@
 import {ReactElement, useContext} from "react";
 import {View, Text, Image} from "react-native";
 import {SessionContext} from "../providers/SessionProvider";
+import ChatMessageType from "../enums/ChatMessageType";
 
 /**
  * Renders a chat message, including the sender's name and avatar.
@@ -11,12 +12,27 @@ import {SessionContext} from "../providers/SessionProvider";
 export default function ChatMessage({message}: { message: ChatMessageSchema }): ReactElement {
     const {user} = useContext(SessionContext)
 
+    /**
+     * Renders the contents of the message, based on its media type.
+     */
+    const renderContents = () => {
+        switch (message.mediaType) {
+            case ChatMessageType.TEXT:
+                return <Text>{message.contents}</Text>
+            case ChatMessageType.IMAGE:
+                // TODO: Allow users to tap on the image to view it in full screen.
+                return <Image source={{uri: message.contents}} style={{width: 200, height: 200}} />
+            default:
+                return <Text>Unknown message type</Text>
+        }
+    }
+
     return (
         <View style={{
             flex: 1,
             alignItems: message.senderId === user?.uid ? "flex-end" : "flex-start",
         }}>
-            <Text>{message.contents}</Text>
+            {renderContents()}
             <Text>{message.createdAt?.toISOString()}</Text>
             <Text>{message.senderName}</Text>
             <Image source={{uri: message.senderAvatar}} style={{width: 50, height: 50}} />
--- src/enums/ChatMessageType.ts
+++ src/enums/ChatMessageType.ts
@@ -1,6 +1,9 @@
+/**
+ * Represents the type of chat messages available.
+ */
 enum ChatMessageType {
-    TEXT= "text",
-    IMAGE = "image",
+    TEXT= "TEXT",
+    IMAGE = "IMAGE",
 }
 
 export default ChatMessageType
