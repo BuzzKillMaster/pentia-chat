c69042bcea6f70ece4609fb42850f90097367afd
Christian Pedersen <chris@buzzkill.dev>
2024-03-07 07:32:20 +0100

    Configure Firebase to send group notifications
    The onMessageCreated cloud function has been refactored to send a notification to subscribers of group topics when a new message is created. It also uses a simple, and therefore unstable, check to see if the contents of the message is an image and acts accordingly.

--- services/functions/src/index.ts
+++ services/functions/src/index.ts
@@ -8,10 +8,25 @@ admin.initializeApp()
  */
 exports.onMessageCreated = functions.firestore
     .document("chat-groups/{groupId}/messages/{messageId}")
-    .onCreate(async (_, context) => {
-        await admin.firestore()
-            .doc("chat-groups/" + context.params.groupId)
-            .update({
-                lastMessageAt: admin.firestore.FieldValue.serverTimestamp()
-            })
+    .onCreate(async (snapshot, context) => {
+        const ref = admin.firestore().doc("chat-groups/" + context.params.groupId)
+
+        await admin.firestore().doc("chat-groups/" + context.params.groupId).update({
+            lastMessageAt: admin.firestore.FieldValue.serverTimestamp()
+        })
+
+        const message = snapshot.data()
+
+        await admin.messaging().send({
+            topic: context.params.groupId,
+            notification: {
+                title: message?.senderName || "New Message",
+                // TODO: Find a better way to make this check, as it can be tricked, for example when users share a URL.
+                body: message?.contents?.startsWith("http")  ? message?.contents || "New Message" : "Sent an image",
+            },
+            data: {
+                groupId: context.params.groupId,
+                groupName: (await ref.get()).get("name")
+            }
+        })
     })
